0aeefe8c123b6b11518ca49f4723ab4feca1770f
Your Name <you@example.com>
2020-02-19 11:03:39 +0000

    add databaseurl

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var campgroundRoutes = require("./routes/campgrounds"),
 	commentRoutes = require("./routes/comments"),
 	indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb+srv://webdev:dreammate@cluster0-o2dmj.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser : true , useUnifiedTopology: true , useFindAndModify : false , useCreateIndex: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser : true , useUnifiedTopology: true , useFindAndModify : false , useCreateIndex: true});
+// mongoose.connect("mongodb+srv://webdev:dreammate@cluster0-o2dmj.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser : true , useUnifiedTopology: true , useFindAndModify : false , useCreateIndex: true});
 app.use(bodyParser.urlencoded({extended : true})); 
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname + "/public"));
